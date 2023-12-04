058bd7add5989d1b8a281634ebde431ef1b48f52
Yago <yagotour@gmail.com>
2023-12-04 21:40:47 +0100

    Añadido Test

---
+++ test.py
@@ -0,0 +1,6 @@
+import unittest
+from fibo import fibonacci
+class TestFibonacci(unittest.TestCase):
+    def test_posicion(self):
+        result = fibonacci(5)
+        self.assertEqual(result, 3)
