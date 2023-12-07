fb3f726a7cf5f927150aab012c43322dcfa339ff
Yago <yagotour@gmail.com>
2023-12-07 22:01:53 +0100

    Ultimos retoques

--- fibo.py
+++ fibo.py
@@ -1,18 +1,19 @@
 #Tarea 1 Yago Touriño Medrano
+#Archivo .py donde está creada la función "fibonacci" perteneciente al primer punto del ejercicio.
 
-def fibonacci(longitud):
+def fibonacci(posicion):
     num1 = 0
     num2 = 1
-    if longitud <= 0:
+    if posicion <= 0:
         return "El número debe ser superior a 0"
-    elif longitud == 1:
+    elif posicion == 1:
         return 0
-    elif longitud == 2:
+    elif posicion == 2:
         return 1
     else:
-        for i in range(longitud - 2):
+        for i in range(posicion - 2):
             resultado = num2+num1
             num1 = num2
             num2 = resultado
         return resultado
-#print(fibonacci(4))
+#print(fibonacci(5))
--- test.py
+++ test.py
@@ -1,7 +1,10 @@
+#Tarea 1 Yago Touriño Medrano
+#Archivo .py donde se llama a la clase "unittest"y se importa la función "fibonacci" del archivo fibo.py
+#Se crea la clase TestFibonacci para verificar que la posición número "5" de la función es "3"
+
 import unittest
 from fibo import fibonacci
 class TestFibonacci(unittest.TestCase):
     def test_posicion(self):
         result = fibonacci(5)
-        self.assertEqual(result, 3)
-
+        self.assertEqual(result, 5, "Error, el resultado del test de la función \"fibonacci\" no es el esperado")
